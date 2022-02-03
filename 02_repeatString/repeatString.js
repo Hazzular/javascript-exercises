12d2c8a185d8101c1af2a86474f6eb5052a686a5
Harriet <h.gislam@hotmail.co.uk>
2022-02-03 17:13:52 +0000

    exercise completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(string, num) {
+   for (var i=1; i<=5;i++){
+       if (num < 0){ return("ERROR")}
+       else {return(string.repeat(num))}
+   }
+}
+console.log(repeatString("hey",-1))
 // Do not edit below this line
 module.exports = repeatString;
+
