ded38d6bd7e6b7d859241ad1be0327e6b15f3a16
Harriet <h.gislam@hotmail.co.uk>
2022-02-03 17:37:59 +0000

    exercise complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    var splitString = string.split("");
+    var reverseArray = splitString.reverse(); 
+    var joinArray = reverseArray.join("");
+    return joinArray;
 
 };
 
+console.log(reverseString("hello"))
 // Do not edit below this line
 module.exports = reverseString;
