d971bf2392f77a8f196de2c301d001aac47090ee
Harriet <h.gislam@hotmail.co.uk>
2022-02-04 10:43:43 +0000

    finished exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
 
-};
+    const removeFromArray = function(arr, ...args) {
+        let a = arr;
+        let b = args;
+        let c = a.filter(d => !b.includes(d));
+        return(c)
+      };
+    
 
+var ar = [1, 2, 3, 4, 5, 6]
+console.log(removeFromArray(ar,5,6))
 // Do not edit below this line
 module.exports = removeFromArray;
